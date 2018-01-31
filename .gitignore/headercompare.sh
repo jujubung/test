15ca181e1945ffce63c0d3f9c0ac3830fa28d3b9
YongGil Choi <jujubung3@naver.com>
2018-01-31 13:38:24 +0900

    Update headercompare.sh

--- .gitignore/headercompare.sh
+++ .gitignore/headercompare.sh
@@ -2,15 +2,12 @@
 
 echo Input your first URL, ex\) http or https://www.example.com/
 read input1
-
 echo Input your second URL, ex\) http or https://www.example.com/
 read input2
-
-#echo $input1
-#echo $input2
-
 url1=$input1
 url2=$input2
+
+#Taking input from the user for target URLs for the comparison. 
 
 echo -e "\n"
 echo "########## petching the headers ############"
@@ -19,14 +16,20 @@ echo -e "\n"
 curl -I "{$url1}" > ./url1.result
 curl -I "{$url2}" > ./url2.result
 
+#Petching Each URL only for response header with "-I" option.
+
 sort url1.result
 sort url2.result
+
+#Sorting the result for comparison
 
 echo -e "\n"
 echo "############################################"
 echo "########## Diff Result Showing #############"
 
 diff -ui <(sort ./url1.result) <(sort ./url2.result) | sort -rb
+
+#Running Diff
 
 rm ./url1.result
 rm ./url2.result
@@ -36,3 +39,5 @@ Note) Result Explain
 "+" Indicates a line in the second URL added.
 "-" Indicates a line in the second URL removed from the first URL.
 Other lines without "+,-" are not changed.
+
+# Showing the result, added Lines, Removed Lines, Dupulicated Lines from Diff Too.
